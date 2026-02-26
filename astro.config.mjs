2f84f71494a836aafdb1583ba07413d1278983b4
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2026-02-26 10:41:18 -0500

    #9 making progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://minyanzayit.org',
-  integrations: [sitemap()]
+  integrations: [sitemap(), icon()]
 });
