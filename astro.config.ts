7c546adb516cb95a305e73d2cc55c3c65df5ea51
Eric Robsky Huntley <ehuntley@mit.edu>
2026-09-22 13:00:30 -0400

    astro.config.mjs > .ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
-
 import icon from 'astro-icon';
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
